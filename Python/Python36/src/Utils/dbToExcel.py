8aada170e948e664286fc59c56cfccb0023b5862
RenXie_MacPro <yeyu0926@hotmail.com>
2017-07-30 22:19:04 +0800

    openpyxl ,列数超过26会有问题

--- Python/Python36/src/Utils/dbToExcel.py
+++ Python/Python36/src/Utils/dbToExcel.py
@@ -1,35 +1,8 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-import pymysql
 import xlwt
 from openpyxl import load_workbook
 from openpyxl import Workbook
-
-
-# 打开数据库连接
-connection = pymysql.connect("localhost", "root", "1121", "python")
-
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = connection.cursor()
-
-# SQL 查询语句
-sql = "select * from python order by lum1"
-
-try:
-    # 执行SQL语句
-    temp = cursor.execute(sql)
-    # 获取所有记录列表
-    rows = cursor.fetchall()
-    print("获取到%d条记录." % temp)
-except:
-    print("Error: unable to fecth data")
-
-# 关闭数据库连接
-connection.close()
-
-filePath = u"/Users/renxie/Code/Python/Python36/src/MySQL/1.xls"
-filePath1 = u"/Users/renxie/Code/Python/Python36/src/MySQL/1.xlsx"
-
 
 def writer(colnum=0, rownum=0, rows=None, sheet=None):
     """
@@ -47,14 +20,6 @@ def writer(colnum=0, rownum=0, rows=None, sheet=None):
             colnum = colnum + 1
         colnum = colnumtmp
         rownum = rownum + 1
-
-
-# xlwt方式写入
-workbook = xlwt.Workbook()
-sheet = workbook.add_sheet('sheet1')
-writer(1, 1, rows, sheet)
-workbook.save(filePath)
-
 
 def writer1(colnum=0, rownum=0, rows=None, sheet=None):
     """
@@ -74,10 +39,3 @@ def writer1(colnum=0, rownum=0, rows=None, sheet=None):
             colnum = colnum + 1
         colnum = colnumtmp
         rownum = rownum + 1
-
-
-# openpyxl方式写入
-workbookNew = Workbook()
-ws = workbookNew.active
-writer1(1, 1, rows, ws)
-workbookNew.save(filePath1)
