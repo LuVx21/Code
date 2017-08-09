b302a11c34c94c0bea01ef4d036e09d9b4fc82ab
RenXie_MacPro <yeyu0926@hotmail.com>
2017-08-09 23:20:04 +0800

    fake-sueragent

---
+++ Python/Python36/src/fake-useragent/demo.py
@@ -0,0 +1,15 @@
+# pip install fake-useragent
+
+from fake_useragent import UserAgent
+ua = UserAgent()
+
+ua.ie
+# Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);
+ua.msie
+# Mozilla/5.0 (compatible; MSIE 10.0; Macintosh; Intel Mac OS X 10_7_3; Trident/6.0)'
+ua['Internet Explorer']
+# Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)
+ua.opera
+# Opera/9.80 (X11; Linux i686; U; ru) Presto/2.8.131 Version/11.11
+ua.chrome
+# Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'
