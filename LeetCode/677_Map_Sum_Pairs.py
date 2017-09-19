165de02cf427da6431bf52d4711690722ff07d28
RenXie_MacPro <yeyu0926@hotmail.com>
2017-09-19 22:29:29 +0800

    677~679

---
+++ LeetCode/677_Map_Sum_Pairs.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+insert.sum方法的类,
+insert实现<string,int>键值对插入,key相同则覆盖
+sum求以指定字符串开头的键的值的和
+'''
+
+class MapSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        
+
+    def insert(self, key, val):
+        """
+        :type key: str
+        :type val: int
+        :rtype: void
+        """
+        
+
+    def sum(self, prefix):
+        """
+        :type prefix: str
+        :rtype: int
+        """
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
---
+++ LeetCode/679_24_Game.py
@@ -0,0 +1,18 @@
+# *-* utf-8 *-*
+
+'''
+数字1~9的4个(可重复),
+判断能否通过+,-,*,/,()运算得到24
+'''
+
+
+class Solution:
+    def judgePoint24(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+
+# solution=Solution()
+# solution.judgePoint24(nums)
