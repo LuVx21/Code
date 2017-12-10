1898ba5962bb5cdecc6e42f014d2a42671b331c5
RenXie <yeyu0926@hotmail.com>
2017-12-10 18:35:32 +0800

    add volatile关键字使用演示

--- java/Spring-Boot/src/test/java/org/luvx/SpringBoot/HelloTests.java
+++ java/Spring-Boot/src/test/java/org/luvx/SpringBoot/HelloTests.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.luvx.SpringBoot.controller.HelloWorldController;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,8 +15,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.example.demo.controller.HelloWorldController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
