5011c918b8a73c69f2ec5b2ce35c50a9101272ae
RenXie <yeyu0926@hotmail.com>
2017-12-17 19:21:27 +0800

    utils

---
+++ java/luvx-utils/src/main/java/org/luvx/commons/MyXxx.java
@@ -0,0 +1,20 @@
+package org.luvx.commons;
+
+public class MyXxx {
+	private String xxx;
+
+	public String getXxx() {
+		return xxx;
+	}
+
+	public void setXxx(String xxx) {
+		this.xxx = xxx;
+	}
+
+	@Override
+	public String toString() {
+		return "MyXxx [xxx=" + xxx + "]";
+	}
+	
+	
+}
---
+++ java/luvx-utils/src/main/java/org/luvx/servlet/AServlet.java
@@ -0,0 +1,27 @@
+package org.luvx.servlet;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AServlet extends BaseServlet {
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	public String add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("add()...");
+		String name = request.getParameter("username");
+		System.out.println("name: " + name);
+		return "f:/index.jsp";
+	}
+
+	public String update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("update()...");
+		return "r:/index.jsp";
+	}
+}
