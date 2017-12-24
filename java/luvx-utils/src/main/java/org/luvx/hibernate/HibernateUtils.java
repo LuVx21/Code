7a1358eb02ecb95ab565210115cb1aff0938d245
RenXie <yeyu0926@hotmail.com>
2017-12-24 22:42:50 +0800

    utils

--- java/luvx-utils/src/main/java/org/luvx/hibernate/HibernateUtils.java
+++ java/luvx-utils/src/main/java/org/luvx/hibernate/HibernateUtils.java
@@ -6,27 +6,66 @@ import org.hibernate.cfg.Configuration;
 
 /**
  * Hibernate框架的工具类
- * @author Administrator
  */
 public class HibernateUtils {
 
-    private static final Configuration CONFIG;
-    private static final SessionFactory FACTORY;
 
-    // 编写静态代码块
-    static{
-        // 加载XML的配置文件
-        CONFIG = new Configuration().configure();
-        // 构造工厂
-        FACTORY = CONFIG.buildSessionFactory();
-    }
+	// 会话工厂，以单例方式管理
+	private static SessionFactory sessionFactory;
 
-    /**
-     * 从工厂中获取Session对象
-     * @return
-     */
-    public static Session getSession(){
-        return FACTORY.openSession();
-    }
+	// ThreadLocal存储session
+	private static ThreadLocal<Session> session = new ThreadLocal<Session>();
+
+
+	// 以单例方式管理sessionFactory
+	static {
+		try {
+			sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			throw new HibernateException("初始化会话工厂失败！");
+		}
+
+	}
+	//得到一个单例的会话工厂
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+	//获取一个新session
+	public static Session openSession(){
+		return sessionFactory.openSession();
+	}
+	
+	//获取当前与线程绑定的session，如果获取不到则创建一个新session并与当前线程绑定
+//	public static Session getCurrentSession() throws HibernateException {
+//		//获取当前线程绑定的session
+//		Session s = (Session) session.get();
+//		if (s == null) {
+//			//创建一个新session
+//			s = sessionFactory.openSession();
+//			//新session并与当前线程绑定
+//			session.set(s);
+//		}
+//		return s;
+//	}
+ 
+	public static Session getCurrentSession() throws HibernateException {
+		return sessionFactory.getCurrentSession(); 
+	}
+	//关闭当前线程绑定的session
+//	public static void closeSession() throws HibernateException {
+//		//获取当前线程绑定的session
+//		Session s = (Session) session.get();
+//		if (s != null){
+//			//关闭session
+//			s.close(); 
+//		}
+//		session.set(null);
+//	}
+	
+	public static void closeSession() throws HibernateException {
+		sessionFactory.getCurrentSession().close();
+	}
+
 
 }
