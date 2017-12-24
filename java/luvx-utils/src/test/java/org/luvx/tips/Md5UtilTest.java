0ad0e44ea3bd89ebf9c181c123fee0c27f0c693d
RenXie <yeyu0926@hotmail.com>
2017-12-24 17:10:41 +0800

    save

---
+++ java/luvx-utils/src/test/java/org/luvx/tips/Md5UtilTest.java
@@ -0,0 +1,11 @@
+package org.luvx.tips;
+
+import org.junit.Test;
+
+public class Md5UtilTest {
+
+    @Test
+    public void md5Test() {
+        System.out.println(Md5Util.encode("1234"));
+    }
+}
