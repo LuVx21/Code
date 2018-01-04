79f7b56e9db4345f145ae527a7b5ada1260deb9a
RenXie <yeyu0926@hotmail.com>
2018-01-04 22:52:27 +0800

    reflect method

--- java/luvx-trial/src/main/java/org/luvx/reflect/InnerClass.java
+++ java/luvx-trial/src/main/java/org/luvx/reflect/InnerClass.java
@@ -31,4 +31,50 @@ public class InnerClass {
             System.out.println("no name");
         }
     };
+
+    /**
+     * 静态方法
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int mul1(int a, int b) {
+        return b * a;
+    }
+
+    /**
+     * 私有
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private int mul(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * 公有
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * 参数为包装类
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public Integer add(Integer a, Integer b) {
+        return a + b;
+    }
+
+
 }
